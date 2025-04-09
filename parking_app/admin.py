bc0b5b63c32150829aa2037ef2433f2766c7a0cd
chiranjevi123 <chiranjeevidabbeti@gmail.com>
2025-04-09 19:12:06 +0530

    Delete parking_app directory

--- parking_app/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import ParkingArea, ParkingSlot, ParkingRecord
-
-admin.site.register(ParkingArea)
-admin.site.register(ParkingSlot)
-admin.site.register(ParkingRecord)
